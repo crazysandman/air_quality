d88c418d9718041de38c1e49eea31a2762b760d2
crazysandman <jakob1997@t-online.de>
2025-05-18 16:08:46 +0200

    update python files and so on

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,14 @@
-test
+from sqlalchemy import Column, Integer, Float, DateTime
+from geoalchemy2 import Geography
+from db import Base
+from datetime import datetime, timezone
+
+class SensorData(Base):
+    __tablename__ = "sensor_data"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sensor_id = Column(Integer, index=True, nullable=False)
+    location = Column(Geography(geometry_type='POINT', srid=4326), nullable=False)
+    pm25 = Column(Float, nullable=True)
+    temperature = Column(Float, nullable=True)
+    timestamp = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
