292197ace9aa601c27ec17acbfad9b11a3751dcb
crazysandman <jakob1997@t-online.de>
2025-09-15 15:18:19 +0200

    Android app improvements: timeout fixes, clickable markers, station details

--- air_quality_app_android/app/src/main/java/de/project/air_quality_app/network/ApiClient.kt
+++ air_quality_app_android/app/src/main/java/de/project/air_quality_app/network/ApiClient.kt
@@ -1,13 +1,22 @@
 package de.project.air_quality_app.network
 
 import de.project.air_quality_app.Constants
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiClient {
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+    
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
